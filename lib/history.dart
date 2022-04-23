50c65c40a7c55be2d58a362f07371db7eeab32b9
khaldaranikhansa <khaldaranikhansae@gmail.com>
2022-04-23 18:19:15 +0700

    add bottom navbar & second historypage

--- lib/history.dart
+++ lib/history.dart
@@ -44,7 +44,9 @@ class _StartHistoryState extends State<StartHistory> {
               icon: Icon(Icons.arrow_back, color: Colors.black,)
               ),   
           actions: [
-            IconButton(onPressed: (){}, icon:  Icon(Icons.arrow_right, color: Colors.black,))
+            IconButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondHistoryPage()));
+            }, icon:  Icon(Icons.arrow_right, color: Colors.black,))
           ],
         ),
 
@@ -156,7 +158,206 @@ class _StartHistoryState extends State<StartHistory> {
 
             ],
           ),
-        ])
+        ]),
+
+        bottomNavigationBar: 
+          BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.pin_drop,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history,
+              color: Colors.blue,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+          ],
+        )
+      )
+    );
+  }
+}
+
+class SecondHistoryPage extends StatefulWidget {
+  const SecondHistoryPage({ Key? key }) : super(key: key);
+
+  @override
+  State<SecondHistoryPage> createState() => _SecondHistoryPageState();
+}
+
+class _SecondHistoryPageState extends State<SecondHistoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text("History",
+            style: TextStyle(color: Colors.black, fontWeight: FontWeight.w600, fontSize: 30),),
+              ),
+          backgroundColor: Color.fromARGB(255, 97, 203, 115),
+          leading: IconButton(onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder: (context) => const HistoryPage()));
+          }, 
+              icon: Icon(Icons.arrow_back, color: Colors.blue,
+              )
+              ),   
+          actions: [
+            IconButton(onPressed: (){}, icon:  Icon(Icons.arrow_right, color: Colors.black,))
+          ],
+        ),
+        body:
+        ListView(
+          children: [
+            Column(
+              children: <Widget>[
+                //1
+                Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  color: Color(0xFFC4C4C4),
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(10),
+                  leading: Image(image: AssetImage('assets/toyota.png',
+                  ),
+                  ),
+                  
+                  title: Text("Toyota Avanza 1.3 E MT",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 16,
+                      ), 
+                        ),
+                  subtitle: Text("P 3821 AA",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16),
+                  ),
+                  trailing: Text("1 hari\nRp 100.000",
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(fontSize: 16),
+                      ),
+                ),
+              ),
+                
+              //2
+              Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  color: Color(0xFFC4C4C4),
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(10),
+                  leading: Image(image: AssetImage('assets/mercedes.png',
+                  ),
+                  ),
+                  
+                  title: Text("Mercedes Benz CL Class 500",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 16,
+                      ), 
+                        ),
+                  subtitle: Text("N 9742 FD",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16),
+                  ),
+                  trailing: Text("3 hari\nRp 450.000",
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(fontSize: 16),
+                      ),
+                ),
+              ),
+
+              //3
+              Card(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  color: Color(0xFFC4C4C4),
+                child: ListTile(
+                  contentPadding: EdgeInsets.all(10),
+                  leading: Image(image: AssetImage('assets/mercedes.png',
+                  ),
+                  ),
+                  
+                  title: Text("Honda Jazz RS CVT",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 16,
+                      ), 
+                        ),
+                  subtitle: Text("W 5433 YY",
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 16),
+                  ),
+                  trailing: Text("3 hari\nRp 440.000",
+                      textAlign: TextAlign.right,
+                      style: const TextStyle(fontSize: 16),
+                      ),
+                ),
+              ),
+
+            ],
+          ),
+        ]),
+        bottomNavigationBar: 
+          BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.pin_drop,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.history,
+              color: Colors.blue,
+              ),
+              label: "",
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person,
+              color: Colors.black,
+              ),
+              label: "",
+              ),
+          ],
+        )
       )
     );
   }
