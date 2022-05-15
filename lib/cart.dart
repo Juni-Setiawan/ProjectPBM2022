1ba14d8b2a27c15d7cf6da77467a303d2458243b
Juni-Setiawan <junissetiawan322@gmail.com>
2022-05-15 09:40:01 +0700

    nambah appbar

--- lib/cart.dart
+++ lib/cart.dart
@@ -1,3 +1,4 @@
+import 'package:CasperCar/home.dart';
 import 'package:flutter/material.dart';
 import 'package:CasperCar/navbar.dart';
 
@@ -11,17 +12,30 @@ class cart extends StatefulWidget {
 class _cartState extends State<cart> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Container(
-          color: Colors.white,
-          child: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const <Widget>[
-                  Text('Cart'),
-                ]),
-          )),
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            "Keranjang",
+            style: TextStyle(
+                color: Colors.black, fontWeight: FontWeight.w600, fontSize: 24),
+          ),
+        ),
+        backgroundColor: Color.fromARGB(255, 97, 203, 115),
+        leading: IconButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomePage(),
+                  ));
+            },
+            icon: Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+            )),
+      ),
     ));
   }
 }
