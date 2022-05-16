e82b74b2fe016bf3a5d009e462b0577c7b28cdbc
221002-Fety <zianafety22@gmail.com>
2022-05-16 17:52:19 +0700

    fix

--- lib/profile.dart
+++ lib/profile.dart
@@ -39,8 +39,8 @@ class NewWidget extends StatelessWidget {
                     borderRadius: BorderRadius.circular(10.0),
                     child: Image.asset(
                       'assets/images/setiawan.jpeg',
-                      width: 100,
-                      height: 100,
+                      width: 90,
+                      height: 90,
                     ),
                   ),
                   Column(
@@ -78,37 +78,37 @@ class NewWidget extends StatelessWidget {
                     ],
                   )
                 ]),
-                const SizedBox(height: 40),
+                const SizedBox(height: 15),
                 CustomButton3(
                   icon: Icons.account_box_outlined,
                   text: 'Setiawan',
                   onPressed: () {},
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 CustomButton3(
                   icon: Icons.card_membership,
                   text: '3525183294324738',
                   onPressed: () {},
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 CustomButton3(
                   icon: Icons.email,
                   text: 'yudi012@gmail.com',
                   onPressed: () {},
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 CustomButton3(
                   icon: Icons.phone_android_outlined,
                   text: '+628310359321',
                   onPressed: () {},
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 CustomButton3(
                   icon: Icons.location_city_outlined,
                   text: 'Jl. Proklamasi Lumajang',
                   onPressed: () {},
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 CustomButton3(
                     icon: Icons.logout,
                     text: 'Logout',
