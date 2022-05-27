572e10ff7fb6633a435e1c280648696baf7964ce
221002-Fety <zianafety22@gmail.com>
2022-05-27 08:03:19 +0700

    benerin edit profi

--- lib/edit_profil.dart
+++ lib/edit_profil.dart
@@ -1,3 +1,4 @@
+import 'package:CasperCar/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:CasperCar/widget/costum_button_profile.dart';
 import 'package:CasperCar/widget/custom_button_edit.dart';
@@ -83,34 +84,159 @@ class EditProfile extends StatelessWidget {
                   ),
                 ],
               ),
-              const SizedBox(height: 30),
-              CostumButton2(
-                  nama: 'Nama', ket: 'Setiawan', onPressed: () {}, width: 140),
-              const SizedBox(height: 20),
-              CostumButton2(
-                  nama: 'No. KTP',
-                  ket: '3525183294324738',
-                  onPressed: () {},
-                  width: 55),
-              const SizedBox(height: 20),
-              CostumButton2(
-                  nama: 'E-Mail',
-                  ket: 'Yudi012@gmail.com',
-                  onPressed: () {},
-                  width: 65),
-              const SizedBox(height: 20),
-              CostumButton2(
-                  nama: 'No. HP',
-                  ket: '+628310359321',
-                  onPressed: () {},
-                  width: 85),
-              const SizedBox(height: 20),
-              CostumButton2(
-                  nama: 'Alamat',
-                  ket: 'JL. Proklamasi Lumajang',
-                  onPressed: () {},
-                  width: 30),
-              const SizedBox(height: 20),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ],
+                ),
+                child: TextField(
+                  cursorColor: Color.fromARGB(255, 20, 20, 20),
+                  decoration: InputDecoration(
+                    hintText: "Nama",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ],
+                ),
+                child: TextField(
+                  cursorColor: Color.fromARGB(255, 20, 20, 20),
+                  decoration: InputDecoration(
+                    hintText: "No. KTP",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ],
+                ),
+                child: TextField(
+                  cursorColor: Color.fromARGB(255, 20, 20, 20),
+                  decoration: InputDecoration(
+                    hintText: "E-Mail",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ],
+                ),
+                child: TextField(
+                  cursorColor: Color.fromARGB(255, 20, 20, 20),
+                  decoration: InputDecoration(
+                    hintText: "No. HP",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 20),
+                padding: EdgeInsets.only(left: 20, right: 20),
+                height: 60,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  boxShadow: [
+                    BoxShadow(
+                        offset: Offset(0, 10),
+                        blurRadius: 50,
+                        color: Color(0xffEEEEEE)),
+                  ],
+                ),
+                child: TextField(
+                  cursorColor: Color.fromARGB(255, 20, 20, 20),
+                  decoration: InputDecoration(
+                    hintText: "Alamat",
+                    enabledBorder: InputBorder.none,
+                    focusedBorder: InputBorder.none,
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return Profile();
+                  }));
+                  // Write Click Listener Code Here.
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 10, right: 10, top: 10),
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  height: 50,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      (Color.fromARGB(255, 97, 203, 115)),
+                      Color.fromARGB(255, 97, 203, 115)
+                    ], begin: Alignment.centerLeft, end: Alignment.centerRight),
+                    borderRadius: BorderRadius.circular(50),
+                    color: Colors.grey[300],
+                    boxShadow: [
+                      BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 50,
+                          color: Color(0xffEEEEEE)),
+                    ],
+                  ),
+                  child: Text(
+                    "Simpan",
+                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ),
+              ),
             ],
           ),
         ));
